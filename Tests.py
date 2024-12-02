69837f2a0d16b14be22e248c5805555fd9c44a36
Bohdan Savin <bogdan07112009@gmail.com>
2024-12-02 23:02:01 +0200

    done

---
+++ Tests.py
@@ -0,0 +1,68 @@
+import os
+
+import pytest
+from httpx import ASGITransport, AsyncClient
+from PIL import Image
+import aiofiles
+
+from app import app
+from app.settings import ALLOWED_EXTENSIONS, ALLOWED_MIME, MAX_FILE_SIZE
+
+
+@pytest.mark.asyncio
+async def test_upload_files_success():
+    test_file_path = "test_image.jpg"
+    image = Image.new("RGB", (100, 100), color=(73, 109, 137))
+    image.save(test_file_path, "JPEG")
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        with open(test_file_path, "rb") as buffer:
+            response = await ac.post(
+                "/file/upload/",
+                files={"files": ("test_image.jpg", buffer, "image/jpeg")},
+            )
+
+    assert response.status_code == 200
+
+    os.remove(test_file_path)
+
+
+@pytest.mark.asyncio
+async def test_upload_files_unsupported_format():
+    test_file_path = "test_file.txt"
+    with open(test_file_path, "wb") as buffer:
+        buffer.write(b"This is a test file.")
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        with open(test_file_path, "rb") as buffer:
+            response = await ac.post(
+                "/file/upload/",
+                files={"files": ("test_file.txt", buffer)},
+            )
+    assert response.status_code == 400
+
+    os.remove(test_file_path)
+
+
+@pytest.mark.asyncio
+async def test_upload_files_exceeds_size():
+    test_file_path = "large_file.jpg"
+    image = Image.new("RGB", (20000, 20000), color=(73, 109, 137))
+    image.save(test_file_path, "JPEG")
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        with open(test_file_path, "rb") as buffer:
+            response = await ac.post(
+                "/file/upload/",
+                files={"files": ("large_file.jpg", buffer, "image/jpeg")},
+            )
+
+    assert response.status_code == 400
+
+    os.remove(test_file_path)
--- app/utils/antivirus.py
+++ app/utils/antivirus.py
@@ -19,6 +19,7 @@ async def virus_check(filename: str, content: bytes) -> dict:
     async with aiohttp.ClientSession() as session:
         form = aiohttp.FormData()
         form.add_field("file", content, filename=filename, content_type="image/png")
+
         async with session.post(url, data=form, headers=headers) as response:
             analyse_id = await response.json()
             result_url = f"https://www.virustotal.com/api/v3/analyses/{analyse_id.get('data').get('id')}"
@@ -33,23 +34,3 @@ async def virus_check(filename: str, content: bytes) -> dict:
                 )
                 logger.info(f"Malicious count {malicious_count}")
                 return malicious_count
-
-
-# async def virus_check(file_path: str) -> dict:
-#     url = "https://www.virustotal.com/api/v3/files"
-#     headers = {
-#         "accept": "application/json",
-#         "x-apikey": VT_KEY,
-#     }
-#     async with aiohttp.ClientSession() as session:
-#         async with aiofiles.open(file_path, "rb") as file:
-
-#             async with session.post(
-#                 url, files={"file": file}, headers=headers
-#             ) as response:
-#                 analyse_id = await response.json()
-#                 result_url = (
-#                     f"https://www.virustotal.com/api/v3/analyses/{analyse_id.get("id")}"
-#                 )
-#             async with session.get(result_url, headers=headers):
-#                 return await response.json()
